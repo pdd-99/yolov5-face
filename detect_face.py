1253715a6850e48856709f2c8d1ef577134473f8
PhanThanhTrung <trung99nb@gmail.com>
2022-04-27 23:15:09 +0700

    update predict batch model

--- detect_face.py
+++ detect_face.py
@@ -5,9 +5,8 @@ from pathlib import Path
 
 import cv2
 import torch
-import torch.backends.cudnn as cudnn
-from numpy import random
 import copy
+import torch.nn.functional as F
 
 from models.experimental import attempt_load
 from utils.datasets import letterbox
@@ -15,7 +14,6 @@ from utils.general import check_img_size, non_max_suppression_face, apply_classi
     strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
-
 
 def load_model(weights, device):
     model = attempt_load(weights, map_location=device)  # load FP32 model
@@ -68,17 +66,98 @@ def show_results(img, xywh, conf, landmarks, class_num):
     cv2.putText(img, label, (x1, y1 - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
     return img
 
+def preprocess_batch(images,
+                     target_size: int = -1, device = 'cpu'):
+        # Resize on the longer side of the image to the target size (keep aspect ratio)
+        batch_size = len(images)
+        
+        resized_images= []
+        all_height = []
+        all_width = []
+        all_scale = []
+        all_index = []
 
+        image_size_dict = {}
+        for idx, image in enumerate(images):
+            print(image.shape)
+            height, width, _ = image.shape
+            _size = f"{height}x{width}"
+            if _size not in image_size_dict:
+                image_size_dict[_size] = [idx]
+            else:
+                image_size_dict[_size].append(idx)
+        
+        # Dynamic batching
+        for key in image_size_dict.keys():
+            # image_height, image_width = list(map(int, key.split('x')))
+            image_height, image_width = int(key.split('x')[0]), int(key.split('x')[1])
+            scale: float = 1.
+            batch = torch.stack([images[idx] for idx in image_size_dict[key]]).to(device).permute(0, 3, 1, 2).float()
+            if target_size != -1:
+                if max(image_height, image_width) > target_size:
+                    print(image.shape)
+                    if image_height >= image_width:
+                        scale = target_size / image_height
+                        new_height = target_size
+                        new_width = int(image_width * scale)
+                    else:
+                        scale = target_size / image_width
+                        new_width = target_size
+                        new_height = int(image_height * scale)
 
-def detect_one(model, image_path, device):
+                    resized_batch = F.interpolate(batch, size=(
+                        new_height, new_width), mode="bicubic", align_corners=False)
+                else:
+                    new_height = image_height
+                    new_width = image_width
+                    resized_batch = batch
+            else:
+                new_height = image_height
+                new_width = image_width
+                resized_batch = batch
+
+            resized_batch = resized_batch/255.0
+
+            resized_images.extend(list(resized_batch))
+            all_index.extend(image_size_dict[key])
+            all_width.append(new_width)
+            all_height.append(new_height)
+            all_scale.extend([scale] * len(image_size_dict[key]))
+        
+        # zip(all_index, resized_images)
+        temp = []
+        for idx in range(len(all_index)):
+            temp.append((all_index[idx], resized_images[idx]))
+ 
+        # zip(all_index, all_scale)
+        temp2 = []
+        for idx in range(len(all_index)):
+            temp2.append((all_index[idx], all_scale[idx]))
+
+        resized_images = [x for _, x in sorted(temp)]
+        all_scale = [x for _, x in sorted(temp2)]
+
+        # Zero padding sequential
+        # max_width = max(all_width)
+        # max_height = max(all_height)
+        batched_tensor = torch.zeros((batch_size, 3, target_size, target_size), device=device).float()
+        for index in range(batch_size):
+            resized_image = resized_images[index]
+            image_size = resized_image.size()
+            image_height, image_width = int(image_size[1]), int(image_size[2])
+            batched_tensor[index, :, :image_height,
+                           :image_width] = resized_image
+        return batched_tensor, all_scale
+
+def detect_one(model, orgimg, device):
     # Load model
-    img_size = 800
-    conf_thres = 0.3
+    img_size = 640
+    conf_thres = 0.7
     iou_thres = 0.5
 
-    orgimg = cv2.imread(image_path)  # BGR
+    # orgimg = cv2.imread(image_path)  # BGR
     img0 = copy.deepcopy(orgimg)
-    assert orgimg is not None, 'Image Not Found ' + image_path
+    assert orgimg is not None, 'fuck you'
     h0, w0 = orgimg.shape[:2]  # orig hw
     r = img_size / max(h0, w0)  # resize image to img_size
     if r != 1:  # always resize down, only resize up if training with augmentation
@@ -92,7 +171,6 @@ def detect_one(model, image_path, device):
     img = img[:, :, ::-1].transpose(2, 0, 1).copy()  # BGR to RGB, to 3x416x416
 
     # Run inference
-    t0 = time.time()
 
     img = torch.from_numpy(img).to(device)
     img = img.float()  # uint8 to fp16/32
@@ -101,12 +179,14 @@ def detect_one(model, image_path, device):
         img = img.unsqueeze(0)
 
     # Inference
-    t1 = time_synchronized()
+    # t1 = time_synchronized()
+    t0 = time.time()
     pred = model(img)[0]
-
+    print(time.time()-t0)
+    t0= time.time()
     # Apply NMS
     pred = non_max_suppression_face(pred, conf_thres, iou_thres)
-
+    print(time.time()-t0)
     print('img.shape: ', img.shape)
     print('orgimg.shape: ', orgimg.shape)
 
@@ -131,18 +211,65 @@ def detect_one(model, image_path, device):
                 class_num = det[j, 15].cpu().numpy()
                 orgimg = show_results(orgimg, xywh, conf, landmarks, class_num)
 
-    cv2.imwrite('result.jpg', orgimg)
+    return orgimg
 
+def detect_batch(model,
+                list_image, device):
+    img_size = 640
+    conf_thres = 0.7
+    iou_thres = 0.5
 
+    output_list = []
+    for image in list_image:
+        output_list.append(torch.tensor(image))
+    
+    batched_tensor, all_scale = preprocess_batch(output_list, img_size, device)
 
+    pred = model(batched_tensor)[0]
+
+    pred = non_max_suppression_face(pred, conf_thres, iou_thres)
+    # import ipdb; ipdb.set_trace()
+    output_list = []
+    for i, image in enumerate(list_image):  # detections per image
+        det = pred[i]
+        print(det)
+        bbox = (det[:,:4]/all_scale[i]).round()
+        conf = det[:,4]
+        landmark = (det[:,5:15]/all_scale[i]).round()
+
+        for j in range(det.size()[0]):
+            xywh = (xyxy2xywh(bbox[j].view(1, 4))).view(-1).tolist()
+            conf = conf[j].cpu().numpy()
+            landmarks = (landmark[j].view(1, 10)).view(-1).tolist()
+            class_num = det[j, 15].cpu().numpy()
+            image = show_results(image, xywh, conf, landmarks, class_num)
+            cv2.imwrite(f'./DEBUG/{time.time()}.jpg', image)
+        output_list.append(image)
+
+    return output_list
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp5/weights/last.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='/Users/miles/Downloads/yolov5n-0.5.pt', help='model.pt path(s)')
     parser.add_argument('--image', type=str, default='data/images/test.jpg', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     opt = parser.parse_args()
     print(opt)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device('cpu')
     model = load_model(opt.weights, device)
-    detect_one(model, opt.image, device)
+    import glob
+    all_image_path = glob.glob('/Users/miles/Downloads/UFDD/UFDD_val/images/*/*.jpg')
+    n= len(all_image_path)
+    list_batch = []
+    output_list =[]
+    import tqdm
+
+    for path in tqdm.tqdm(all_image_path):
+        image = cv2.imread('/Users/miles/Techainer/face_detection/yolov5-face/data/images/test.jpg')
+        # rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        if len(list_batch)==4:
+            output_list = detect_batch(model,list_batch, device)
+            for image in output_list:
+                cv2.imwrite(f'./DEBUG/{time.time()}.jpg', image)
+            list_batch = []
+        list_batch.append(image)
